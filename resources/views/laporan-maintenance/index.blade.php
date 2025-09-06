6494ec9d0d593a01dedcd56ab8f90b01e1bd6460
Bagas K <bagaskm36@gmail.com>
2025-09-06 21:56:48 +0700

    lap. maintenance jquery

--- resources/views/laporan-maintenance/index.blade.php
+++ resources/views/laporan-maintenance/index.blade.php
@@ -37,7 +37,13 @@
     </div>
 </div>
 
-@push('scripts')
+@push('script')
+<!-- jQuery HARUS di atas DataTables -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+<!-- DataTables -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <script>
     $(document).ready(function() {
         $('#laporanMaintenanceTable').DataTable({
