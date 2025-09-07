32e6113491840148be1b859ba3258b454a3874a5
Bagas K <bagaskm36@gmail.com>
2025-09-07 12:56:27 +0700

    adjustment for web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\KalibrasiController;
+use App\Http\Controllers\MaintenanceController;
+use App\Http\Controllers\InstalasiController;
+use App\Http\Controllers\LaporanKalibrasiController;
+use App\Http\Controllers\LaporanMaintenanceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +20,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Resource routes
+    Route::resource('kalibrasi', KalibrasiController::class);
+    Route::resource('maintenance', MaintenanceController::class);
+    Route::resource('instalasi', InstalasiController::class);
+    
+    // Rute khusus untuk halaman "Cari Laporan"
+    // Ini akan menampilkan view tanpa data, hanya form pencarian
+    Route::get('/laporan/cari', [LaporanKalibrasiController::class, 'searchOnly'])->name('laporan.search_only');
+    
+    Route::get('/laporan/download/{laporan}', [LaporanKalibrasiController::class, 'download'])->name('laporan.download');
+    // Rute untuk menampilkan hasil pencarian
+    // Ini akan mengambil data dan menampilkannya di halaman khusus pencarian
+    Route::get('/laporan/hasil-cari', [LaporanKalibrasiController::class, 'searchResults'])->name('laporan.search_results');
+    
+    // Rute untuk manajemen laporan (tambah, edit, hapus, dan tampilkan semua)
+    Route::resource('laporan', LaporanKalibrasiController::class)->except(['index']);
+    Route::get('/laporan', [LaporanKalibrasiController::class, 'index'])->name('laporan.index');
+    
+    Route::resource('laporan-maintenance', LaporanMaintenanceController::class);
 });
 
 require __DIR__.'/auth.php';
