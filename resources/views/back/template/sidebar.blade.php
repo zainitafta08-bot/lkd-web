9a65d2278c41c51f5c97805866a98e503f8d78bc
Bagas K <bagaskm36@gmail.com>
2025-09-07 13:00:21 +0700

    sidebar dashboard adjustment

--- resources/views/back/template/sidebar.blade.php
+++ resources/views/back/template/sidebar.blade.php
@@ -1,3 +1,7 @@
+@php
+    use Illuminate\Support\Facades\Auth;
+@endphp
+
 <div class="app-menu navbar-menu bg-white">
     <div class="navbar-brand-box bg-white">
         <a href="#" class="logo logo-dark">
@@ -26,7 +30,7 @@
             <span class="d-flex align-items-center gap-2">
                 <img class="rounded header-profile-user" src="assets/images/users/avatar-1.jpg" alt="Header Avatar">
                 <span class="text-start">
-                    <span class="d-block fw-medium sidebar-user-name-text">ade</span>
+                    <span class="d-block fw-medium sidebar-user-name-text">{{ Auth::user()->name ?? 'Guest' }}</span>
                     <span class="d-block fs-14 sidebar-user-name-sub-text"><i class="ri ri-circle-fill fs-10 text-success align-baseline"></i> <span class="align-middle">Online</span></span>
                 </span>
             </span>
@@ -40,37 +44,42 @@
                 <li class="menu-title"><span data-key="t-menu">Menu</span></li>
 
                 <li class="nav-item">
+                    <a class="nav-link menu-link" href="{{ route('dashboard') }}">
+                        <i class="ri-dashboard-3-line"></i> <span data-key="t-dashboard">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link menu-link" href="{{ route('laporan.search_only') }}">
                         <i class="ri-search-line"></i> <span data-key="t-search">Cari Laporan</span>
                     </a>
                 </li>
             
-                <li class="menu-title"><span>alat</span></li>
+                <li class="menu-title"><span>Alat</span></li>
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="{{ route('kalibrasi.index') }}">
-                        <i class="ri-folder-line"></i> <span>Kalibrasi</span>
+                        <i class="ri-tools-line"></i> <span>Kalibrasi</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="{{ route('maintenance.index') }}">
-                        <i class="ri-folder-line"></i> <span>Maintanance</span>
+                        <i class="ri-settings-3-line"></i> <span>Maintenance</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="{{ route('instalasi.index') }}">
-                        <i class="ri-folder-line"></i> <span>Instalasi</span>
+                        <i class="ri-install-line"></i> <span>Instalasi</span>
                     </a>
                 </li>
 
                 <li class="menu-title"><span>Laporan</span></li>
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="{{ route('laporan.index') }}">
-                        <i class="ri-notification-3-line"></i> <span>Laporan Kalibrasi</span>
+                        <i class="ri-file-text-line"></i> <span>Laporan Kalibrasi</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="{{ route('laporan-maintenance.index') }}">
-                        <i class="ri-notification-3-line"></i> <span>Laporan Maintenance</span>
+                        <i class="ri-file-list-3-line"></i> <span>Laporan Maintenance</span>
                     </a>
                 </li>
             </ul>
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,7 @@
+@extends('back.template.index')
+@section('title' ,'Dashboard')
+@section('content')
+<div class="container mt-5">
+    <h1 class="mb-4">Dashboard</h1>
+</div>
+@endsection
